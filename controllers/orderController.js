4e4e73e9f9bf64c1435ba57d2c285a213ad657e9
sheyndi <nechamatog@gmail.com>
2025-06-12 20:33:41 +0300

    oooooo13

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -69,7 +69,7 @@ export const addOrder = async (req, res) => {
             return res.status(400).json({ title: "cannot add order", message: "id_user is not exist" })
 
         for (const prod of body.products) {
-            if(!prod.id_gift_in_GIFTS||!prod.quantity||prod.name||!prod.price)
+            if (!prod.id_gift_in_GIFTS || !prod.quantity || !prod.name || !prod.price)
                 return res.status(400).json({ title: "missing parameters", message: "Not all required product parameters were received" })
             let gift = await GIFTS.findById(prod.id_gift_in_GIFTS)
             if (!gift)
