f1690040db412beaaf122b443ae4ae0174164b34
sheyndi <nechamatog@gmail.com>
2025-07-14 14:56:46 +0300

    public

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,11 +2,11 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) =>{
-        cb(null, './src/public/images/products');
+        cb(null, './public/images/products');
     },
-    
+
     filename: (req, file, cb) =>{
-        cb(null, Date.now());
+        cb(null, Date.now().toString() + '.' + file.originalname.split('.').pop());
     }
 });
 
